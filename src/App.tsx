0ebaca12ab16aae79adcc49fd02fce6784dd0854
PierrePocheron <pierre.pocheron@gmail.com>
2023-02-16 11:56:56 +0100

    refactor: clear useless files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+import "./style.scss";
 
 function App() {
   return (
