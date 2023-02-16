c7aeaf6e8899c9b8332af89402d47f2a445d4e4d
PierrePocheron <pierre.pocheron@gmail.com>
2023-02-16 09:38:56 +0100

    chore: init router

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,13 @@
+const LoginPage = () => {
+  return (
+    <main className="main">
+      <div className="container">
+        <section className="wrapper">
+          <h1>Login Page</h1>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
