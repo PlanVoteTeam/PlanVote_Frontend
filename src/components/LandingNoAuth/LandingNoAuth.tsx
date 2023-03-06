d35144cd3b84f8e239bf2c883e5f29c536341675
Julie Miler <julie.miler@ynov.com>
2023-03-06 16:08:13 +0100

    [ADD] recupérer valeur de l'événèment

--- src/components/LandingNoAuth/LandingNoAuth.tsx
+++ src/components/LandingNoAuth/LandingNoAuth.tsx
@@ -1,21 +1,33 @@
+import { useState } from 'react';
 import './LandingNoAuth.scss'
 
 const LandingNoAuth = () => {
-    return (
-        <div>
-          <div className='landing__background'>
-            <div className='px-5 is-flex is-flex-direction-column is-justify-content-center is-align-items-center landing__wrapper'>
-              <h1 className='mb-2 title is-1'>Plan & Vote</h1>
-              <p className='mb-4 content is-small has-text-weight-light has-text-centered'>Organiser des vacances entre amis c'est compliqué. C'est pourquoi nous avons conçu Plan&nbsp;&&nbsp;Vote, la solution idéale pour planifier des vacances.</p>
-              <div className='is-flex is-flex-direction-row'>
-                <input className="input is-primary mr-3" type="text" placeholder="Nom de l'évènement"></input>
-                <button className="button is-primary">Création</button>
-              </div>
-            </div>
+
+  const [nameEvent, setNameEvent] = useState<string>();
+
+  const handleChangeEvent = (event: any) => {
+    setNameEvent(event.currentTarget.value)
+  }
+
+  const handleSumbit = () => {
+    alert(nameEvent)
+  }
+
+  return (
+      <div>
+        <div className='landing__background'>
+          <div className='px-5 is-flex is-flex-direction-column is-justify-content-center is-align-items-center landing__wrapper'>
+            <h1 className='mb-2 title is-1'>Plan & Vote</h1>
+            <p className='mb-4 content is-small has-text-weight-light has-text-centered'>Organiser des vacances entre amis c'est compliqué. C'est pourquoi nous avons conçu Plan&nbsp;&&nbsp;Vote, la solution idéale pour planifier des vacances.</p>
+            <form className='is-flex is-flex-direction-row' onSubmit={handleSumbit}>
+              <input className="input is-primary mr-3" type="text" placeholder="Nom de l'évènement" onChange={handleChangeEvent}></input>
+              <button className="button is-primary" type='submit'>Création</button>
+            </form>
           </div>
         </div>
-    );
-  };
+      </div>
+  );
+};
   
-  export default LandingNoAuth;
+export default LandingNoAuth;
   
