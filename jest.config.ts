bd2106024b4f90d77055c5c7b568517314b19a0c
Julie Miler <julie.miler@ynov.com>
2023-03-09 14:23:45 +0100

    [DELETE] TU

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.tsx?$": "ts-jest",
-    },
-    moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
-  }
