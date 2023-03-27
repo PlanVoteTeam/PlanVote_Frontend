6c2acc8764a43fa6321c98e1073425ba262d088b
Julie Miler <julie.miler@ynov.com>
2023-03-27 16:41:26 +0200

    [ADD] affichage de la liste des destination

--- src/components/AddDestination/AddDestination.tsx
+++ src/components/AddDestination/AddDestination.tsx
@@ -1,40 +1,44 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { apiUrl } from '../../../config';
+import './AddDestination.scss'
 
 interface Participant {
     _id: number;
     name: string;
-  }
+}
 
 const AddDestination = () => {
     const { eventId } = useParams();
-    
+
     const [listDestination, setListDestination] = useState<Array<string>>([]);
     const [nameDestination, setNameDestination] = useState<string>('');
     const [isError, setIsError] = useState<boolean>(false);
 
 
     useEffect(() => {
-        setListDestination(['rome', 'strasbourg', 'nice'])
-        /*fetch(apiUrl + ``, { mode: "cors" })
-          .then((blob) => blob.json())
-          .then((response) => {
-            console.log(response)
-            setListDestination(response);
-          })
-          .catch((error) => {
-            console.error(error);
-          });*/
-      }, [eventId]);
+        fetch(apiUrl + `events/${eventId}`, { mode: "cors" })
+            .then((blob) => blob.json())
+            .then((response) => {
+                const participants = response.participants;
+                participants.map((participant: { destinations: any; }) => {
+                    participant.destinations.map((dest: { name: any; }) => {
+                        listDestination.push(dest.name)
+                    })
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }, [eventId]);
 
-      function getCurrentParticipantLocalStorage(): Participant | null {
+    const getCurrentParticipantLocalStorage = (): Participant | null => {
         const storedParticipant = localStorage.getItem("currentParticipant");
         if (storedParticipant) {
-          return JSON.parse(storedParticipant);
+            return JSON.parse(storedParticipant);
         }
         return null;
-      }
+    }
 
     const handleChangeEvent = (event: any) => {
         setNameDestination(event.currentTarget.value)
@@ -55,39 +59,57 @@ const AddDestination = () => {
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             },
             body: formData.toString(),
-            mode: "cors"     
+            mode: "cors"
         })
-          .then((response) => response.json())
-          .then((response) => {
-            if (response.sucess === false) {
-                setIsError(true)
-            }
-            else {
-                setIsError(false)
-            }
-          })
-          .catch((error) => {
-            setIsError(true);
-          });
+            .then((response) => response.json())
+            .then((response) => {
+                if (response.sucess === false) {
+                    setIsError(true)
+                }
+                else {
+                    setIsError(false)
+                    const updateListDestination = [...listDestination, nameDestination]
+                    setListDestination(updateListDestination)
+                    setNameDestination("")
+                }
+            })
+            .catch((error) => {
+                setIsError(true);
+            });
     }
 
-  return (
-      <div>
-        {isError ? 
-            (
-              <div className="notification is-danger">
-                Une erreur est survenu, veuillez réessayer plus tard.
-              </div>
-            ) : (<div></div>)
-          }
-        {listDestination.map(dest => <p key={dest}> {dest} </p>)}
-       <form className='is-flex is-flex-direction-row' onSubmit={handleSumbit}>
-              <input className="input is-primary mr-3" type="text" placeholder="Rome" onChange={handleChangeEvent}></input>
-              <button className="button is-primary" type='submit'>Ajout destination</button>
+    return (
+        <div>
+            {isError ?
+                (
+                    <div className="notification is-danger">
+                        Une erreur est survenu, veuillez réessayer plus tard.
+                    </div>
+                ) : (<div></div>)
+            }
+            <div className="addDestination__wrapper-card">
+            {listDestination.map(dest => 
+                <div className="card">
+                    <div className="card-image">
+                        <figure className="image is-128x128">
+                            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <div className="card-content">
+                        <div className="content">
+                            {dest}
+                        </div>
+                    </div>
+                </div>
+                )}
+            </div>
+
+            <form className='is-flex is-flex-direction-row addDestination__form' onSubmit={handleSumbit}>
+                <input className="input is-primary mr-3" type="text" placeholder="Rome" value={nameDestination} onChange={handleChangeEvent}></input>
+                <button className="button is-primary" type='submit'>Ajout destination</button>
             </form>
-      </div>
-  );
+        </div>
+    );
 };
-  
+
 export default AddDestination;
-  
