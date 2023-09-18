41c0942bf50db17bd05151b4c73cc4a652b11489
Pierre POCHERON <pierre.pocheron@gmail.com>
2023-09-18 18:42:12 +0200

    chore: define URL of Backend

--- config.ts
+++ config.ts
@@ -1,1 +1,3 @@
-export const apiUrl: string = import.meta.env.VITE_LOCAL_API_URL || "";
+export const apiUrl: string =
+  import.meta.env.VITE_LOCAL_API_URL ||
+  "https://planvotebackend.onrender.com/ ";
