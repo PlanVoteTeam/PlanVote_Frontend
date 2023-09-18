f3c6b0c0f528fe1544250e97830632a436d45d30
Pierre POCHERON <pierre.pocheron@gmail.com>
2023-09-18 19:04:09 +0200

    fix: if condition

--- config.ts
+++ config.ts
@@ -1,2 +1,3 @@
-export const apiUrl: string =
-  import.meta.env.VITE_LOCAL_API_URL || "https://planvotebackend.onrender.com/";
+export const apiUrl: string = import.meta.env.VITE_LOCAL_API_URL
+  ? import.meta.env.VITE_LOCAL_API_URL
+  : "https://planvotebackend.onrender.com/";
