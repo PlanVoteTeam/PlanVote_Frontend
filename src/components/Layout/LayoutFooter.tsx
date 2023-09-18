ebff24717e59e2b6c4fc2771e7de1f012d856e4f
Pierre POCHERON <pierre.pocheron@gmail.com>
2023-09-18 15:37:13 +0200

    feat(ui): improve ui of footer

--- src/components/Layout/LayoutFooter.tsx
+++ src/components/Layout/LayoutFooter.tsx
@@ -1,19 +1,26 @@
-import { Link } from "react-router-dom";
+import "./LayoutFooter.scss";
 
 function LayoutFooter() {
   return (
     <footer className="footer">
-      <div className="container">
+      <div className="footerContainer">
         <div className="columns">
           <div className="column">
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <a className="footerCredit" href="/">
+                  © Plan & Vote - 2023
+                </a>
               </li>
             </ul>
           </div>
           <div className="column">
-            <p>© 2023</p>
+            <ul className="footerLinkList">
+              <li className="footerLink">TERMES</li>
+              <li className="footerLink">POLITIQUE RELATIVE AUX COOKIES</li>
+              <li className="footerLink">POLITIQUE DE CONFIDENTIALITÉ</li>
+              <li className="footerLink">LICENCE</li>
+            </ul>
           </div>
         </div>
       </div>
